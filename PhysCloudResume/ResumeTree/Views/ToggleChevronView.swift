a4a50060ff190e127a0e31da273d160410ce8807
Christopher Culbreath <cc@physicscloud.net>
2025-05-08 18:06:08 -0500

    Uncommited unknown

--- PhysCloudResume/ResumeTree/Views/ToggleChevronView.swift
+++ PhysCloudResume/ResumeTree/Views/ToggleChevronView.swift
@@ -9,15 +9,11 @@ import SwiftUI
 
 struct ToggleChevronView: View {
     @Binding var isExpanded: Bool
-    let toggleAction: () -> Void
 
     var body: some View {
         Image(systemName: "chevron.right")
             .rotationEffect(.degrees(isExpanded ? 90 : 0))
             .animation(.easeInOut(duration: 0.1), value: isExpanded)
             .foregroundColor(.primary)
-            .onTapGesture {
-                toggleAction()
-            }
     }
 }
